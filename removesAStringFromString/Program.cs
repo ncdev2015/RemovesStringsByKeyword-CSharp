cb1e6ba8697b8e33989615b6fd44993df4e63ade
ncdev2015 <ncdev2015@gmail.com>
2021-12-29 10:59:31 -0300

    Agregar archivos de proyecto.

---
+++ removesAStringFromString/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace removesAStringFromString
+{
+    class Program
+    {
+        static string removesStringByKeyword(string toRemove, String input)
+        {
+            var arrayInput = input.Split(" ");
+            var newString = "";
+
+            foreach (var item in arrayInput)
+            {
+                if (item.Contains(toRemove))
+                    continue;
+
+                newString += item + " ";
+            }
+
+            return newString.Remove(newString.Length-1);
+        }
+
+        static void Main(string[] args)
+        {
+            string strIn = "This is a sample of string, this is to remove: AnyCharRemoveMeOthreString RemoveMe";
+            Console.WriteLine(removesStringByKeyword("RemoveMe", strIn));
+        }
+    }
+}
